4ec5f46f31e89782b27f670ce1384a4eada37dab
hafidjs <afidzpratama@gmail.com>
2025-11-19 22:38:15 +0700

    Introduction Theme

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,3 +1,4 @@
+import 'package:alquran/app/contants/color.dart';
 import 'package:alquran/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
@@ -28,14 +29,23 @@ class IntroductionView extends GetView<IntroductionController> {
                 style: TextStyle(fontSize: 16),
               ),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 70),
             Container(
               width: 300,
               height: 300,
               child: Lottie.asset("assets/lotties/animasi-quran.json"),
             ),
             SizedBox(height: 30),
-            ElevatedButton(onPressed: () => Get.offAllNamed(Routes.HOME), child: Text("MULAI")),
+            ElevatedButton(
+              onPressed: () => Get.offAllNamed(Routes.HOME),
+              child: Text(
+                "GET STARTED",
+                style: TextStyle(
+                  color: Get.isDarkMode ? appWhite : appWhite,
+                ),
+              ),
+               style: ElevatedButton.styleFrom(backgroundColor: Get.isDarkMode ? appWhite : appPurple, padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15)),
+            ),
           ],
         ),
       ),
